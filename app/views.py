585994d1d37ba8e6e770f3060fae97887b561189
geethasai2527 <geethasai2527@gmail.com>
2023-07-28 12:08:21 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def conditions(request):
-    d={'name':'geetha','age':12}
+    d={'a':10,'b':20}
     return render(request,'conditions.html',context=d)
